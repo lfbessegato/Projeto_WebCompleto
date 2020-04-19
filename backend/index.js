c179b8e7543c599c4abcdd54cf7303661e1105b5
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-19 10:46:24 -0300

    Backend - Users e Routes e configuração no Consign

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const consign = require('consign')
 // Consig() -> gerenciar as dependencias dos middlewares
 consign ()
     .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
 
 app.listen(3000, () => {
