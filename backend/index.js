46c36b68f444aa5e5a784be1f2f33f8a253bcdf5
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-19 10:29:05 -0300

    Backend - configuração do Consign para os Middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 const app = require('express')()
+const consign = require('consign')
+
+// Consig() -> gerenciar as dependencias dos middlewares
+consign ()
+    .then('./config/middlewares.js')
+    .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...')
