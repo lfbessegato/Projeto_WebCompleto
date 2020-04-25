781eb9b8d232787522632a7ff27ab388476ba1c4
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-25 11:19:01 -0300

    Backend - informacoes de Conexao no ./env

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,13 +1,9 @@
+const { db } = require('./.env')
 // Update with your config settings.
-
 module.exports = {
   
   client: 'postgresql',
-  connection: {
-    database: 'knowledge',
-    user:     'postgres',
-    password: 'marcia'
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
