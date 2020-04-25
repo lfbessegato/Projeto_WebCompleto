6bac608061704b8ae83ef2dc93f6cb688c6b0bcc
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-25 11:48:52 -0300

    Backend - Validar Cadastro de Administradores

--- backend/api/user.js
+++ backend/api/user.js
@@ -13,6 +13,10 @@ module.exports = app => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
 
+        // Para validar que somente admin podem acessar
+        if(!req.originalUrl.startsWith('/users')) user.admin = false
+        if(!req.user || !req.user.admin) user.admin = false
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
