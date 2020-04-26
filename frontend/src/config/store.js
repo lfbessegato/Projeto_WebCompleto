c1d1635a070428292ca4bbaa61772c979021be1b
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-25 21:48:00 -0300

    Frontend - Visibilidade do Menu (Toggle)

---
+++ frontend/src/config/store.js
@@ -0,0 +1,22 @@
+// para fazer a relação entre os componentes
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: { 
+        isMenuVisible: true
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if (isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+
+            
+        }
+    }
+})
