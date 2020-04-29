8f6da914626b5b105c5997a2bd8c3c5959aeef1b
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-28 23:52:20 -0300

    Frontend - Artigos por Categoria #3

--- frontend/src/components/article/ArticlesByCategory.vue
+++ frontend/src/components/article/ArticlesByCategory.vue
@@ -4,7 +4,7 @@
             :main="category.name" sub="Categoria" />
         <ul>
             <li v-for="article in articles" :key="article.id">
-                {{ article.name }}
+               <ArticleItem :article="article" />
             </li>
         </ul>
         <div class="load-more">
@@ -19,9 +19,10 @@
 import { baseApiUrl } from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle'
+import ArticleItem from './ArticleIem'
 export default {
     name: 'ArticlesByCategory',
-    components: { PageTitle },
+    components: { PageTitle, ArticleItem },
     data: function() {
         return {
             category: {},
