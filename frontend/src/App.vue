3b4bdd9033d28f34d5db582bcd3bdba1216c0311
Luciano Bessegato <lfrbessegato@gmail.com>
2020-04-30 23:10:13 -0300

    Frontend - Autenticação: Alterando Store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+	<div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
 
 		<Header title="Cod3r - Base de Conhecimento" 
-			:hideToggle="false" 
-			:hideUserDropDown="false" />
-		<Menu />
+			:hideToggle="!user" 
+			:hideUserDropDown="!user" />
+		<Menu v-if="user"/>
 		<Content />
 		<Footer />
 
@@ -21,7 +21,7 @@ import Footer from "@/components/template/Footer"
 export default {
 	name: "App",
 	components: { Header, Menu, Content, Footer},
-	computed: mapState(['isMenuVisible'])
+	computed: mapState(['isMenuVisible', 'user'])
 }
 </script>
 
